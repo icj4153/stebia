9a31e822677883631ae70d4c56981d5ed2afa675
임창준 <icj7297@gmail.com>
2023-06-30 09:04:36 +0900

    1158_yosepus

--- 9012_PS.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-N = int(sys.stdin.readline())
-
-for _ in range(N):
-    cmd = sys.stdin.readline()
-    left_cnt = 0
-    right_cnt = 0
-    for i in range(len(cmd)):
--- Data_Structure/1158_yosepus.py
+++ Data_Structure/1158_yosepus.py
@@ -1,27 +1,25 @@
-import queue, sys
+import sys
+from collections import deque
 
 n, k = tuple(map(int,sys.stdin.readline().split()))
 
-in_queue = queue.Queue()
-result = queue.Queue()
+in_queue = deque()
+result = deque()
 
 for i in range(n):
-    in_queue.put(i+1)
+    in_queue.append(i+1)
 
-while(in_queue.qsize() >= 3):
+while(len(in_queue) != 0):
     for i in range(k-1):
-        in_queue.put(in_queue.get())
-    result.put(in_queue.get())
-
-result.put(in_queue.get())
-result.put(in_queue.get())
+        in_queue.append(in_queue.popleft())
+    result.append(in_queue.popleft())
 
 
 print("<",end="")
 for i in range(n):
     if i == n-1:
-        print(result.get(),end="")
+        print(result.popleft(),end="")
     else:
-        print(result.get(),end=", ")
+        print(result.popleft(),end=", ")
 
 print(">",end="")
